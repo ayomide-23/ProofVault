5b61f016b09080030c5aacfef3d3aa0a48b25e07
Aderinto Ayomide <aderintoayomide204@gmail.com>
2026-07-16 00:42:32 +0100

    first commit at 16th of july 2026

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App(){
+  return(
+    <div className="flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-red-500">Welcome to ProofVault</h1>
+    </div>
+  )
+}
